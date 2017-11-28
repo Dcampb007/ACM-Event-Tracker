156b2d49f311ef5067c712e5778a8670aa36bd18
DavidAwogbemila <david.awogbemila@bison.howard.edu>
2017-11-28 11:31:15 -0500

    pictures for about site page and links to hu and acm, purely cosmetic additons really

--- src/Containers/AboutThisSite.js
+++ src/Containers/AboutThisSite.js
@@ -15,6 +15,8 @@ class AboutThisSite extends Component {
                 <h1>{"About this site"}</h1>
                 <p> {"This site is a portal for Howard university students to use to keep up to date with HU-ACM's activities and programs."
                 }</p>
+                <a href="https://www.howard.edu" target={"_blank"}><img src={require("../Images/hu_circle.png")}/></a>
+                <a href="https://www.acm.org/about-acm/about-the-acm-organization" target="_blank"><img src={require("../Images/acm_circle.png")}/></a>
             </div>
         </div>);
     }
